c3d452fa234c0587ff2fb598523e40a2ab1076b2
Yashrif Hasib <33043549+yhasib6487@users.noreply.github.com>
2018-12-05 13:55:56 -0500

    new method

--- src/Triangle.java
+++ src/Triangle.java
@@ -19,20 +19,14 @@ public class Triangle extends Shape {
     public double degrees() { return 180; }
 
     public boolean isIsosceles() {
-        if ( this.a == this.b || this.a == this.c || this.c == this.b )
-            return true;
-        return false;
+        return ( isValidTriangle() && ( this.a == this.b || this.a == this.c || this.c == this.b ) );
     }
 
     public boolean isRightTriangle() {
-        if ( ( this.a * this.b / 2 ) == area() || ( this.c * this.b / 2 ) == area() || ( this.a * this.c / 2 ) == area() )
-            return true;
-        return false;
+        return ( isValidTriangle() && ( ( this.a * this.b / 2 ) == area() || ( this.c * this.b / 2 ) == area() || ( this.a * this.c / 2 ) == area() ) );
     }
 
     public boolean isValidTriangle() {
-        if ( ( this.a + this.b ) < this.c || ( this.b + this.c ) < this.a || ( this.c + this.a ) < this.b )
-            return false;
-        return true;
+        return !( ( this.a + this.b ) < this.c || ( this.b + this.c ) < this.a || ( this.c + this.a ) < this.b );
     }
 }
