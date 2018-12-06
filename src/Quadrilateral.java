3746d1089c197434db13951754f0677f8d20b6ed
Yashrif Hasib <33043549+yhasib6487@users.noreply.github.com>
2018-12-06 13:27:21 -0500

    new method

--- src/Quadrilateral.java
+++ src/Quadrilateral.java
@@ -1,8 +1,5 @@
 public abstract class Quadrilateral extends Shape {
-    private double side1;
-    private double side2;
-    private double side3;
-    private double side4;
+    private double side1, side2, side3, side4;
 
     public Quadrilateral( String name, double side1, double side2, double side3, double side4 ) {
         super( name );
@@ -12,21 +9,11 @@ public abstract class Quadrilateral extends Shape {
         this.side4 = side4;
     }
 
-    public double perimeter() {
-        return this.side1 + this.side2 + this.side3 + this.side4;
-    }
+    public double perimeter() { return this.side1 + this.side2 + this.side3 + this.side4; }
     public double degrees() { return 360; }
 
-    public double getSide1() {
-        return this.side1;
-    }
-    public double getSide2() {
-        return this.side2;
-    }
-    public double getSide3() {
-        return this.side3;
-    }
-    public double getSide4() {
-        return this.side4;
-    }
+    public double getSide1() { return this.side1; }
+    public double getSide2() { return this.side2; }
+    public double getSide3() { return this.side3; }
+    public double getSide4() { return this.side4; }
 }
